e31be91ee0851a7a9d211508cd9768bfced2adf1
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-11-28 14:43:36 -0500

    commit tree d84bd40e09fe77b2d33d387ab10923229c1c15a3

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,16 @@
 class Review < ActiveRecord::Base
+    # a review belongs to a game
+    belongs_to :game
+     # self is the review instance
+  end
+
+
+#   class Review < ActiveRecord::Base
+#     # a review belongs to a game
+#     def game
+#       # self is the review instance
+#       Game.find(self.game_id)
+#     end
   
-end
+#   end
+  
